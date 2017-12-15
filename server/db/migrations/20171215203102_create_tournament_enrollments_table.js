729dbe0d21caa7adc737f8a5c275cbd50ada6ba3
kevin <kevin.rph.lee@gmail.com>
2017-12-15 21:28:12 +0000

    populated migrate files

--- server/db/migrations/20171215203102_create_tournament_enrollments_table.js
+++ server/db/migrations/20171215203102_create_tournament_enrollments_table.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('tournament_enrollments', function (table) {
+    table.increments('id').primary();
+    table.integer('user_id');
+    table.integer('team_id');
+    table.integer('tournament_id');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('tournament_enrollments');
+
 };
--- server/db/migrations/20171215203252_create_teams_table.js
+++ server/db/migrations/20171215203252_create_teams_table.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('teams', function (table) {
+    table.increments('id').primary();
+    table.integer('tournament_id');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('teams');
+
 };
