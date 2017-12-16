44812a2be8eff1cba5093ab20ce27626dc6971cb
kevin <kevin.rph.lee@gmail.com>
2017-12-16 00:30:31 +0000

    additional error checking

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,6 +9,15 @@ module.exports = (knex, bcrypt, cookieSession) => {
   router.post("/new", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
+    const battlenetID = req.body.battlenet_id;
+
+    console.log(email + ' ' + password + ' ' + battlenetID)
+    //error checking
+    if(!email || !password || !battlenetID){
+      console.log('empty param!');
+      res.sendStatus(400);
+      return;
+    }
     //Checking if user already exists, if user exists, DO NOT create it
     knex
       .select("email")
@@ -18,7 +27,7 @@ module.exports = (knex, bcrypt, cookieSession) => {
         console.log(results);
         if(results.length === 0){
           knex
-          .insert({email: email, password: bcrypt.hashSync(password, 10), battlenet_id: req.body.battlenet_id})
+          .insert({email: email, password: bcrypt.hashSync(password, 10), battlenet_id: battlenetID})
           .into('users')
           .then(()=>{});
           req.session.email = email;
@@ -33,6 +42,13 @@ module.exports = (knex, bcrypt, cookieSession) => {
   router.post("/login", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
+
+    //error checking
+    if(!email || !password){
+      res.sendStatus(400);
+      return;
+    }
+
     //Checking if user already exists, if user does not exist, throw back a 404
     knex
       .select("email", "password")
