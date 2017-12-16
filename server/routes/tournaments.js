94ddd78116b4855c4659245792b6730361a60f45
kevin <kevin.rph.lee@gmail.com>
2017-12-16 22:43:31 +0000

    created check user count function

--- server/routes/tournaments.js
+++ server/routes/tournaments.js
@@ -5,6 +5,26 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
+  /**
+   * Checks the # of players enrolled in a tournament. Returns true if it's full, false if there is still room
+   *
+   * @param  string no_of_teams  the # of teams in this tournament
+   * @return boolean             true if the tournament
+   */
+  function checkUserCount(noOfTeams, tournamentID){
+     knex
+      .select("id")
+      .from("tournament_enrollments")
+      .where({id : tournamentID})
+      .then((result) => {
+        if((noOfTeams * 6) === result.length){
+          return true;
+        } else {
+          return false;
+        }
+    });
+  }
+
   //creates new tournament
   router.post("/new", (req, res) => {
     //Checking if tournament already exists, if user exists, DO NOT create it
@@ -22,6 +42,24 @@ module.exports = (knex) => {
           res.sendStatus(200);
         } else{
           res.sendStatus(400);
+        }
+    });
+  });
+
+    //creates new tournament
+  router.post("/:id", (req, res) => {
+    //Checking if tournament already exists, it doens't, send 404
+    //TO DO: check with front end to see what data they send down
+    knex
+      .select("name", "no_of_teams")
+      .from("tournaments")
+      .where({id : req.params.id})
+      .then((results) => {
+        console.log('Results ',results);
+        if(results.length === 0){
+          res.sendStatus(400);
+        } else{
+
         }
     });
   });
