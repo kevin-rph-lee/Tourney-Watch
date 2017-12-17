5019164002d63a72cb8268e18e4b05bdb64cdf4c
kevin <kevin.rph.lee@gmail.com>
2017-12-17 00:09:45 +0000

    added boilerplate functions for sort users and seed teams

--- server/routes/tournaments.js
+++ server/routes/tournaments.js
@@ -25,6 +25,47 @@ module.exports = (knex) => {
     });
   }
 
+  /**
+   * Creates teams and seeds players
+   * @param  array players an array of players in the tournament
+   * @return {[type]}         [description]
+   */
+  function seedPlayers(tournamentID, players, noOfTeams){
+    sortPlayers(players);
+    for(let i = 0; i < noOfTeams; i ++){
+      knex
+        .insert({tournament_id: tournamentID})
+        .into('teams')
+        .then(()=>{
+          //TO DO, SEED THE ALYERS TO THIS TEAM
+        });
+    }
+  }
+
+  function sortPlayers(players){
+    //TO DO: sort the players
+  }
+
+
+
+  //Shows dashboard
+  router.get("/:id", (req, res) => {
+    //Checking if tournament already exists, it doens't, send 404
+    //TO DO: check with front end to see what data they send down
+    knex
+      .select("name", "no_of_teams")
+      .from("tournaments")
+      .where({id : req.params.id})
+      .then((results) => {
+        if(results.length === 0){
+          res.sendStatus(404);
+        } else {
+          //TO DO: render front end dashboard
+        }
+    });
+  });
+
+
   //creates new tournament
   router.post("/new", (req, res) => {
     //Checking if tournament already exists, if user exists, DO NOT create it
@@ -46,23 +87,36 @@ module.exports = (knex) => {
     });
   });
 
-    //creates new tournament
-  router.post("/:id", (req, res) => {
+
+  //Starts tournament
+  router.post("/:id/start", (req, res) => {
     //Checking if tournament already exists, it doens't, send 404
     //TO DO: check with front end to see what data they send down
     knex
-      .select("name", "no_of_teams")
+      .select("id")
       .from("tournaments")
       .where({id : req.params.id})
       .then((results) => {
-        console.log('Results ',results);
         if(results.length === 0){
-          res.sendStatus(400);
-        } else{
+          res.sendStatus(404);
+        } else {
+
+          //TO DO: start tournament
+          //knex state to get the players
+          //of those players, run seedPlayers(players)
+
 
         }
-    });
+      });
   });
+
+
+
+
+
+
+
+
 
   return router;
 }
