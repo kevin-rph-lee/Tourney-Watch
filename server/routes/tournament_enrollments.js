12f58240824eaca16073bca58094553f50a86c57
Lilian Shih <l.shih@outlook.com>
2017-12-17 00:35:37 +0000

    added function to rank players best roles using time played of each role class

--- server/routes/tournament_enrollments.js
+++ server/routes/tournament_enrollments.js
@@ -3,15 +3,44 @@
 const express = require('express');
 const router  = express.Router();
 
-module.exports = (knex, owjs, _) => {
+module.exports = (knex, owjs) => {
 
+  const offenseHeroes = ['doomfist', 'genji', 'mccree', 'pharah', 'soldier:_76', 'sombra', 'tracer'];
+  const defenseHeroes = ['bastion', 'hanzo', 'junkrat', 'mei', 'torbjörn', 'widowmaker'];
+  const tankHeroes = ['d.va', 'orisa', 'reinhardt', 'roadhog', 'winston', 'zarya'];
+  const supportHeroes = ['ana', 'lúcio', 'mercy', 'moira', 'symmetra', 'zanyatta'];
+
+  function roleTimes(data, heroNames) {
+    let totalTime = 0;
+    Object.keys(data.quickplay.heroes).reduce((sum, key) => {
+      const arrayOfTimes = [];
+      for (const i in heroNames) {
+        if (data.quickplay.heroes[`${heroNames[i]}`]) {
+          arrayOfTimes.push(data.quickplay.heroes[`${heroNames[i]}`].time_played);
+        }
+      }
+      return totalTime = arrayOfTimes.reduce((a, b) => a + b);
+    }, 0);
+    return totalTime;
+  };
+
+  function sortTime(data) {
+    let sorted = '';
+    const playerTimeStats = [
+      {role: 'offense', time : roleTimes(data, offenseHeroes)},
+      {role: 'defense', time : roleTimes(data, defenseHeroes)},
+      {role: 'tank', time : roleTimes(data, tankHeroes)},
+      {role: 'support', time : roleTimes(data, supportHeroes)}
+    ];
+   return playerTimeStats.sort((a, b) => { return b.time - a.time });
+  }
+
+  
   //user registers
   router.post("/new", (req, res) => {
-    const firstHero = 'tracer'
-    const secondHero = req.body.secondHero
+    // overwatch api insists on all lowercase
     //TO DO!!!!!!!!!!!!!!! get params properly
     //Checking if user already exists, if user exists, DO NOT create it
-    console.log('before', firstHero);
     knex
       .select("battlenet_id")
       .from("users")
@@ -22,6 +51,27 @@ module.exports = (knex, owjs, _) => {
         } else{
           res.send(owjs.getAll('pc', 'us', results[0].battlenet_id)
               .then((data) => {
+                // prints the amount of seconds a person spends as healer
+                const totalHealTime = Object.keys(data.quickplay.heroes).reduce((sum, key) => {
+                  if (data.quickplay.heroes[key].healing_done) {
+                    return sum + data.quickplay.heroes[key].time_played;
+                  }
+                  return sum;
+                }, 0);
+
+                const roleRanks = sortTime(data);
+
+                console.log(roleRanks[0].role)
+                console.log(roleRanks[1].role)
+
+
+
+                const healsPerSecond = data.quickplay.global.healing_done / totalHealTime * 100;
+                // may not be accurate as healers can also damage...could add up all dmg done by 
+                // those heros that did not have the healing_done stat ??
+                const dmgPerSecond = data.quickplay.global.all_damage_done / (data.quickplay.global.time_played - totalHealTime);
+
+                const playerLevel = data.profile.level;
                 // prints array of all player's hero information listed
                 // let playerHeroStats = Object.keys(data.quickplay.heroes).map((key) => {
                 //     return {
@@ -31,16 +81,16 @@ module.exports = (knex, owjs, _) => {
                 //       'tot_time': data.quickplay.heroes[key].time_played
                 //     };
                 //   })
-                return knex('tournament_enrollments').insert({
-                  'id': params,
-                  'user_id': params,
-                  'team_id': params,
-                  'tournament_id': params,
-                  'first_hero_stat': data.quickplay,
-                  'first_hero_rank': null,
-                  'second_hero_stat': data.quickplay,
-                  'second_hero_rank': null
-                })
+                // knex('tournament_enrollments').insert({
+                //   'id': params,
+                //   'user_id': params,
+                //   'team_id': params,
+                //   'tournament_id': params,
+                //   'first_role': roleRanks[0].role,
+                //   'first_role_rank': null,
+                //   'second_role': roleRanks[1].role,
+                //   'second_role_rank': null
+                // })
               })
           )
         }
