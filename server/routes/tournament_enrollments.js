c58bc38d046ff20a100748923ca39a0a134829bb
Lilian Shih <l.shih@outlook.com>
2017-12-17 19:05:16 +0000

    fixed knex insert statement

--- server/routes/tournament_enrollments.js
+++ server/routes/tournament_enrollments.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (knex, owjs) => {
-
+  // overwatch api insists on all lowercase
   const offenseHeroes = ['doomfist', 'genji', 'mccree', 'pharah', 'soldier:_76', 'sombra', 'tracer'];
   const defenseHeroes = ['bastion', 'hanzo', 'junkrat', 'mei', 'torbjörn', 'widowmaker'];
   const tankHeroes = ['d.va', 'orisa', 'reinhardt', 'roadhog', 'winston', 'zarya'];
@@ -50,9 +50,9 @@ module.exports = (knex, owjs) => {
     return data.quickplay.global.all_damage_done / (data.quickplay.global.time_played - totalTimeHealing);
   }
 
-  //user registers
+  // new enrollment to tournament
   router.post("/new", (req, res) => {
-    // overwatch api insists on all lowercase
+    
     //TO DO!!!!!!!!!!!!!!! get params properly
     //Checking if user already exists, if user exists, DO NOT create it
     knex
@@ -67,21 +67,23 @@ module.exports = (knex, owjs) => {
               .then((data) => {
                 const roleRanks = sortTimePlayed(data);
 
-                // knex('tournament_enrollments').insert({
-                  // 'id': params,
-                  // 'user_id': params,
-                  // 'team_id': params,
-                  // 'tournament_id': params,
-                  // 'level': data.profile.level,                 
-                  // 'first_role': roleRanks[0].role,
-                  // 'first_role_time_played': roleRanks[0].time
-                  // 'second_role': roleRanks[1].role,
-                  // 'second_role_time_played': roleRanks[1].time,
-                  // 'medal_gold': data.quickplay.global.medals_gold,
-                  // 'medal_silver': data.quickplay.global.medals_silver,
-                  // 'medal_bronze': data.quickplay.global.medals_bronze,
-                  // 'games_won': data.quickplay.global.games_won
-                // })
+                console.log('inserting into database');
+
+                return knex('tournament_enrollments').insert({
+                  'id': req.body.id,
+                  'user_id': req.body.userID,
+                  'team_id': null,
+                  'tournament_id': req.body.tournID,
+                  'level': data.profile.level,                 
+                  'first_role': roleRanks[0].role,
+                  'first_role_time_played': roleRanks[0].time,
+                  'second_role': roleRanks[1].role,
+                  'second_role_time_played': roleRanks[1].time,
+                  'medal_gold': data.quickplay.global.medals_gold,
+                  'medal_silver': data.quickplay.global.medals_silver,
+                  'medal_bronze': data.quickplay.global.medals_bronze,
+                  'games_won': data.quickplay.global.games_won
+                })
               })
           )
         }
