a8eeeecdaacf1d744cc6d861e8aca58bb3b517cf
Mochirii <melpcsmith@gmail.com>
2017-12-18 20:44:05 +0000

    ajax requests finished and tested

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -2,10 +2,16 @@ $(document).ready(function () {
 
     $("#registration-form").on('submit', function (event) {
         event.preventDefault();
+        var formData = {
+            'email': $('input[id=entry-email]').val(),
+            'battlenet_id': $('input[id=entry-battlenet]').val(),
+            'password': $('input[id=entry-password]').val(),
+
+        };
         $.ajax({
             type: "POST",
             url: "/users/new",
-            data: data,
+            data: formData,
             success: function () {
                 $("#registration-form").html("You have successfully registered!");
             }
@@ -14,10 +20,14 @@ $(document).ready(function () {
 
     $("#login-form").on('submit', function (event) {
         event.preventDefault();
+        var formData = {
+            'email': $('input[id=entry-email]').val(),
+            'password': $('input[id=entry-password]').val(),
+        };
         $.ajax({
             type: "POST",
             url: "/users/login",
-            data: data,
+            data: formData,
             success: function () {
                 $("#login-form").html("You are now logged in!");
             }
@@ -26,10 +36,15 @@ $(document).ready(function () {
 
     $("#new-tournament-form").on('submit', function (event) {
         event.preventDefault();
+        var formData = {
+            'no_of_teams': +$('select[id="entry-team"]').val(),
+            'name': $('input[id=entry-name]').val(),
+            'description': $('input[id=entry-description]').val(),
+        };
         $.ajax({
             type: "POST",
             url: "/tournaments/new",
-            data: data,
+            data: formData,
             success: function () {
                 $("#new-tournament-form").html("Tournament Created!");
             }
