8c58deca1070226111fc25a00476134e41880621
Mochirii <melpcsmith@gmail.com>
2017-12-18 07:09:09 +0000

    ajax requests started

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,3 +1,38 @@
-$(() => {
+$(document).ready(function () {
 
-});
+    $("#registration-form").on('submit', function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "/users/new",
+            data: data,
+            success: function () {
+                $("#registration-form").html("You have successfully registered!");
+            }
+        })
+    })
+
+    $("#login-form").on('submit', function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "/users/login",
+            data: data,
+            success: function () {
+                $("#login-form").html("You are now logged in!");
+            }
+        })
+    })
+
+    $("#new-tournament-form").on('submit', function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "/tournaments/new",
+            data: data,
+            success: function () {
+                $("#new-tournament-form").html("Tournament Created!");
+            }
+        })
+    })
+})
