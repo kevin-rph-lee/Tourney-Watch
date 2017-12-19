35a9b90e49a65b39c45cb62c9f7185cfb19afff4
kevin <kevin.rph.lee@gmail.com>
2017-12-19 21:59:50 +0000

    added brackets column and seed

--- server/db/seeds/tournaments.js
+++ server/db/seeds/tournaments.js
@@ -5,7 +5,36 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('tournaments').insert([
-        {name: 'Test Tournament', description: 'My tournament for testing', no_of_teams: 8}
+        {
+          name: 'Test Tournament',
+          description: 'My tournament for testing',
+          no_of_teams: 8,
+          brackets:
+          `{
+            "teams": [
+                [
+                    { name: "1", flag: "in" },
+                    { name: "2", flag: "in" },
+                ],
+                [
+                    { name: "3", flag: "in" },
+                    { name: "4", flag: "in" },
+                ],
+                [
+                    { name: "5", flag: "in" },
+                    { name: "6", flag: "in" }
+                ],
+                [
+                    { name: "7", flag: "in" },
+                    { name: "8", flag: "in" },
+                ],
+
+
+            ],
+
+            results: [[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]],
+          }`
+        }
       ]);
     });
 };
