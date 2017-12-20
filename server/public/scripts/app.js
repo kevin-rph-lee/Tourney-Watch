ccda01f135798ffdb9745b601a3d4a5bf5a79dab
Lilian Shih <l.shih@outlook.com>
2017-12-20 01:49:14 +0000

    updated scripts

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -12,9 +12,6 @@ $(document).ready(function () {
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
             </div>`)})
-        // console.log(card)
-        // $(".card-container").html(card)
-       
     }
 
     function loadCards() {
