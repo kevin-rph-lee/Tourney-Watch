d74d00af92e4a47036f6db6cba0e03edd623dcc9
Lilian Shih <l.shih@outlook.com>
2017-12-22 01:03:02 +0000

    fixed playersEnrolled

--- server/routes/tournaments.js
+++ server/routes/tournaments.js
@@ -18,7 +18,6 @@ module.exports = (knex, _) => {
     const teamCount = teamArray.length;
     const maxPlayerOffset = playerCount - (playerCount % teamCount);
     let ascending = true;
-
     for (let p = 0; p < maxPlayerOffset; p += teamCount) {
       if(ascending) {
         for (let t = 0; t < teamCount; t++) {
@@ -47,6 +46,9 @@ module.exports = (knex, _) => {
       if (key[roleChoice] === 'support') {
         return count ++;
       }
+      // TO DO: possible refactor? 
+      (key[roleChoice] === "support") ? count++ : 0;
+
     });
     return count;
   }
@@ -88,6 +90,12 @@ module.exports = (knex, _) => {
       .then(() => {});
   }
 
+  /**
+   * Gets each team's roster
+   * 
+   * @param {integer} tournamentID from req params
+   * @returns {array}
+   */
   function getTeamRoster(tournamentID){
     return knex
      .select("tournaments.name", "users.battlenet_id", "team_id", "level", "games_won", "medal_gold", "medal_silver", "medal_bronze")
@@ -101,15 +109,20 @@ module.exports = (knex, _) => {
      });
   }
 
-  function countPlayersEnrolled(tournamentID){
-    return knex("tournament_enrollments")
+  /**
+   * Gets a list of all players enrolled in an tournament
+   * 
+   * @param {integer} tournamentID from req params
+   * @returns {array} 
+   */
+  function playersEnrolled(tournamentID){
+    return knex
+      .select("users.battlenet_id", "level", "games_won", "medal_gold", "medal_silver", "medal_bronze")
+      .from("tournament_enrollments")
+      .innerJoin("users", "users.id", "tournament_enrollments.user_id")
       .where({tournament_id: tournamentID})
-      .count("id")
       .then((result) => {
-        // ASK KEVIN: instead of going through array, we can just do result.length?
-        console.log('i am result in counter', result)
-        console.log('i should be returning', result[0].count)
-        return result[0].count;
+        return result
       });
   }
 
@@ -120,7 +133,6 @@ module.exports = (knex, _) => {
       .from("tournaments")
       .where({id: 1})
       .then((results) => {
-        console.log(results[0]);
         res.json(results[0]);
       });
   });
@@ -150,7 +162,6 @@ module.exports = (knex, _) => {
         // If the tournament name does not exist, create new line in tournaments
         // and creates new lines in teams (based on # of teams needed)
         if(results.length === 0) {
-          console.log(req.session)
           knex
             .insert({name: name, no_of_teams: teamCount, description: description, creator_user_id: req.session.userID, is_started: false})
             .into('tournaments')
@@ -182,12 +193,11 @@ module.exports = (knex, _) => {
       .orderBy("team_id", "ascd")
       .then((playerStats) => {
         const teamRoster = _.groupBy(playerStats, "team_id");
-        // console.log(teamRoster);
         res.send(teamRoster);
       });
   });
 
-  //Updates bracket data in the DB
+  // Updates bracket data in the DB
   router.post("/update", (req, res) => {
     console.log('Updating DB brackets');
     console.log(req.body.tournamentID + req.body.bracketData);
@@ -197,34 +207,54 @@ module.exports = (knex, _) => {
         .then(() => {console.log('Bracket data updated')});
   });
 
-  //TODO : get a coutn of enrolled ppl and pass it down
   router.get("/:id", (req, res) => {
     const tournamentID = parseInt(req.params.id);
     // TODO: FIX, WILL NOT CONSOLE LOG, BUUUT DOES NOT ERROR OUT ANYMORE WHEN A STRING IS USED
     if (!Number.isInteger(tournamentID)) {
       console.log('not a vaid id')
       return res.sendStatus(404);
-    } else {
-      return knex
-      .select("id", "is_started", "creator_user_id")
+    } 
+
+    return knex
+      .select("id", "is_started", "creator_user_id", "no_of_teams", "name")
       .from("tournaments")
       .where({id: tournamentID})
-      .then(async (results) => {
-        if(results.length === 0){
-          console.log("inside selct")
-          return res.sendStatus(400);
+      .then( async (results) => {
+        const enrolledCount = await playersEnrolled(tournamentID);
+        const started = results[0].is_started;
+        const teamCount = results[0].no_of_teams;
+        const creatorUserID = results[0].creator_user_id;
+        const tournamentName = results[0].name;
+        const isReady = (enrolledCount.length === teamCount * 6);
+
+        if (isReady && started) {
+          res.render("tournament_view", {
+            teamRoster: getTeamRoster(tournamentID), 
+            playerCount: enrolledCount.length, 
+            email: req.session.email,
+            isOwner: creatorUserID === req.session.userID ? "true" : "false",
+            started: started,
+            tournamentName: tournamentName
+          })
         } else {
-            const playerCount = await countPlayersEnrolled(tournamentID);
-            console.log('HEEEEEEEEY', playerCount);
-            return res.render("tournament_view", {
-              teamRoster: getTeamRoster(tournamentID), 
-              playerCount: playerCount, 
+          if (req.session.id === creatorUserID) {
+            res.render("tournament_staging", {
+              playerCount: enrolledCount,
               email: req.session.email,
-              creatorUserId: req.session.id
-            });
+              tournamentName: tournamentName,
+              teamCount: teamCount,
+              isReady: isReady
+            })
+          } else {
+            res.render("tournament_notready", {
+              tournamentName: tournamentName,
+              playerCount: enrolledCount.length,
+              teamCount: teamCount,
+              email: req.session.email,
+            })
+          }
         }
       });
-    }
   });
 
   router.post("/:id/start", (req, res) => {
@@ -259,7 +289,6 @@ module.exports = (knex, _) => {
                 .from("teams")
                 .where({tournament_id: tournamentID})
                 .then((teamArray) => {
-                  // console.log(teamArray);
                   initializeBrackets(teamArray, results[0].no_of_teams, tournamentID);
                   const teamAssigned = assignPlayersToTeams(playersArray, teamArray);
                   assignToTeams(teamAssigned);
@@ -269,7 +298,6 @@ module.exports = (knex, _) => {
         }
       });
   });
-
-
+  
   return router;
 };
