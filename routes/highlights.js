9a6125814ce2c06d24e18a82c8e4e0415f464a4d
kevin <kevin.rph.lee@gmail.com>
2018-01-03 21:51:36 +0000

    added function which gets youtube ID from a URL

--- routes/highlights.js
+++ routes/highlights.js
@@ -11,7 +11,9 @@ module.exports = (knex) => {
    * @return {[STRING]}     youtube ID
    */
   function getYoutubeID(url){
-    VID_REGEX = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+
+    const VID_REGEX = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+
     return (url.match(VID_REGEX)[1]);
   }
 
@@ -26,7 +28,8 @@ module.exports = (knex) => {
       });
   });
 
-  router.post("/:id/", (req, res) => {
+  router.post("/:id/new", (req, res) => {
+
     knex
       .select('id')
       .from("tournaments")
@@ -36,7 +39,7 @@ module.exports = (knex) => {
           res.sendStatus(404);
         } else {
           knex
-            .insert({name: req.body.name, url: req.body.url, tournament_id: req.params.id})
+            .insert({name: req.body.name, url: getYoutubeID(req.body.url), tournament_id: req.params.id})
             .into('highlights')
             .returning('id')
             .then((results)=>{
@@ -46,25 +49,6 @@ module.exports = (knex) => {
       });
   });
 
-  router.post("/:id/new", (req, res) => {
-    knex
-      .select('id')
-      .from("tournaments")
-      .where({id : req.params.id})
-      .then((results) => {
-        if(results.length === 0){
-          res.sendStatus(404);
-        } else {
-          knex
-            .insert({name: req.body.name, url: req.body.url, tournament_id: req.params.id})
-            .into('highlights')
-            .returning('id')
-            .then((results)=>{
-              res.sendStatus(200);
-            });
-        }
-      });
-  });
 
   router.post("/:id/delete", (req, res) => {
     knex
