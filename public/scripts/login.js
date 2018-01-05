05ad27831a28f76b824b2595d09cde67eb6e39f5
Chris <chris.northwood@hotmail.com>
2018-01-04 16:17:21 -0800

    Alert fade works in highlights adding, but bug exists where only the first alert fades.

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -51,11 +51,12 @@ $(document).ready(function () {
         `)
       }
     });
+
     window.setTimeout(function() {
       $(".alert").fadeTo(500, 0).slideUp(500, function(){
           $(this).remove(); 
       });
-  }, 3000);
+  }, 5000);
   });
 
 });
--- public/scripts/tournament_view.js
+++ public/scripts/tournament_view.js
@@ -426,6 +426,13 @@ $(document).ready(function () {
         `)
       });
     });
+
+  window.setTimeout(function() {
+    $(".alert").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove(); 
+    });
+  }, 5000);
+
       modalManageHighlights.style.display = "block";
     });
   }
