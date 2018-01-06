f24eb04237084a36dcfc7c62d27620c3c424aba8
Lilian Shih <l.shih@outlook.com>
2018-01-05 16:21:45 -0800

    hardcoded chart in tournament view for manager only

--- public/scripts/tournament_view.js
+++ public/scripts/tournament_view.js
@@ -1,45 +1,5 @@
 $(document).ready(function () {
 
-  $(document).load("")
-
-  var ctx = document.getElementById("myChart");
-  var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero:true
-              }
-          }]
-      }
-    }
-  });
-
   function renderTeamCards(teamRoster) {
     const teamNames = Object.keys(teamRoster)
     $(".tournamentheader").append(`
@@ -108,8 +68,6 @@ $(document).ready(function () {
   }
 
   loadCards();
-
-
 
   // Share button functionality
   $("[data-toggle='toggle']").click(function() {
@@ -452,6 +410,43 @@ $(document).ready(function () {
     }
   });
 
+  const ctx = document.getElementById("myChart");
+  const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: teamNames,
+      datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero:true
+              }
+          }]
+      }
+    }
+  });
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
