bd722f58ea373fac2d71daf3e2833e62dc65d290
kevin <kevin.rph.lee@gmail.com>
2018-01-07 21:34:07 +0000

    setup ajax template

--- public/scripts/tournament_new.js
+++ public/scripts/tournament_new.js
@@ -19,7 +19,31 @@ $(document).ready(function () {
     const name = $('#entry-name').val();
     const description = $('#entry-description').val();
     const date = $('#datepicker').val();
-    console.log(name, description, date);
+
+
+    $.ajax({
+      url: '/users/new',
+      data: {email: email, password: password, battlenet: battlenet},
+      method: 'POST'
+    }).done(() => {
+      //Redirects to the index
+      window.location.replace(`/users/${userID}`);
+    }).catch((err) => {
+      $('.register-alert').append(`
+      <div class="alert alert-warning alert-dismissible fade show" role="alert">
+      <strong>OOPS!</strong> ${err.responseText}
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      </div>
+      `)
+      window.setTimeout(function() {
+        $(".alert").fadeTo(500, 0).slideUp(500, function(){
+          window.location.replace('/users/new');
+        });
+      }, 4000);
+    });
+
 
   });
 
