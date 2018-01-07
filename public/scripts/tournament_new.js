f22503287af90191701ee7524553ad1515d428cb
kevin <kevin.rph.lee@gmail.com>
2018-01-07 21:24:41 +0000

    added date column to tournaments & date to seed

--- public/scripts/tournament_new.js
+++ public/scripts/tournament_new.js
@@ -10,30 +10,18 @@ $(document).ready(function () {
     `)
     window.setTimeout(function() {
       $(".alert").fadeTo(500, 0).slideUp(500, function(){
-          $(this).remove(); 
+          $(this).remove();
       });
     }, 3000);
   }
 
-  // will need to pull in tournamentID somehow to make this work
-  // $("#new-tournament-form").on('submit', function (event) {
-  //   event.preventDefault();
-  //   var formData = {
-  //     'no_of_teams': +$('select[id="entry-team"]').val(),
-  //     'name': $('input[id=entry-name]').val(),
-  //     'description': $('input[id=entry-description]').val(),
-  //     'twitch_channel': $('input[id=entry-channel]').val()
-  //   };
-  //   console.log(formData)
-  //   $.ajax({
-  //     type: "POST",
-  //     url: "/tournaments/new",
-  //     data: formData,
-  //     success: function () {
-  //       $("#new-tournament-form").html("Tournament Created!");
-  //       throw location.href = `http://localhost:8080/tournaments/${tournamentID}`;
-  //     }
-  //   })
-  // })
-  
+  $('.submit').click(function(e){
+    const name = $('#entry-name').val();
+    const description = $('#entry-description').val();
+    const date = $('#datepicker').val();
+    console.log(name, description, date);
+
+  });
+
+
 });
